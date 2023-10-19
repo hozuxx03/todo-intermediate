cbfbac3030cabb14a94729694f0f745992ed362f
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-19 16:15:44 +0900

    2回目

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,60 @@ import { Button } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
 export const App = () => {
-  const [text, setText] = useState("");
-  const [addText, setAddText] = useState("");
+  // Todoを入力欄に入力された文字をstateで管理
+  const [todoText, setTodoText] = useState("");
+
+  // 現在入力済みのTodoリストをstateで管理
+  const [todos, setTodos] = useState<any[]>([]);
+
+  // Todoを入力欄に何か文字が入力されたらその入力された文字に変更
+  const onChangeTodoText = (e: any) => setTodoText(e.target.value);
 
   // 追加ボタンを押した時に
-  const onClickAddText = () => {
-    setAddText(text);
-    setText("");
+  const onClickAdd = () => {
+    console.log({ todoText });
+
+    // もしTodoを入力欄が空だったら処理をreturnする
+    if (todoText === "") return;
+    const newTodos: { id: any; text: any; status: string } = {
+      id: uuidv4(),
+      text: { todoText },
+      status: "作業中",
+    };
+
+    // 入力したTodoを現在入力済みのTodoリストの配列に追加
+    setTodos((newTodos) => [...todos, todoText]);
+
+    // Todoを入力欄を空白に変更
+    setTodoText("");
+  };
+
+  // 削除ボタンがおされたら
+  const onClickDelete = (id: any) => {
+    const newTodos = [...todos];
+    newTodos.splice(id, 1);
+    setTodos(newTodos);
   };
   return (
     <>
       <Box bg="blue.500" w="100%" p={15} color="white">
         Todo List
       </Box>
+
       <div className="input-area">
         <Input
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          value={todoText}
+          onChange={onChangeTodoText}
           htmlSize={50}
           width="90%"
           variant="outline"
           placeholder="Todoを入力"
           size="md"
         />
-        <Button onClick={onClickAddText} colorScheme="blue" variant="solid">
+        <Button onClick={onClickAdd} colorScheme="blue" variant="solid">
           追加
         </Button>
       </div>
@@ -40,7 +68,22 @@ export const App = () => {
           <option value="完了">完了</option>
         </Select>
       </div>
-      <div>{addText}</div>
+      <div>
+        <ul>
+          {todos.map((id, text, status) => (
+            <React.Fragment key={id}>
+              <li>{text}</li>
+              <Button
+                onClick={() => {
+                  onClickDelete(id);
+                }}
+              >
+                削除
+              </Button>
+            </React.Fragment>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
