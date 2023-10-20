502a90abde4e6a81f8a07159b656d6a572e4ce1a
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-20 14:38:33 +0900

    4回目

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,47 @@ import { Input } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
 
+// Todoリストの型を定義
+type TodoType = {
+  id: string;
+  text: string;
+  status: string;
+};
+
 export const App = () => {
   // Todoを入力欄に入力された文字をstateで管理
   const [todoText, setTodoText] = useState("");
 
   // 現在入力済みのTodoリストをstateで管理
-  const [todos, setTodos] = useState<any[]>([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
 
   // Todoを入力欄に何か文字が入力されたらその入力された文字に変更
   const onChangeTodoText = (e: any) => setTodoText(e.target.value);
 
   // 追加ボタンを押した時に
-  const onClickAdd = () => {
-    console.log({ todoText });
-
+  const onClickAdd = (todo: TodoType) => {
     // もしTodoを入力欄が空だったら処理をreturnする
     if (todoText === "") return;
-    const newTodos: { id: any; text: any; status: string } = {
+    const newTodos = {
       id: uuidv4(),
       text: todoText,
       status: "作業中",
     };
 
     // 入力したTodoを現在入力済みのTodoリストの配列に追加
-    todos.push(newTodos);
+    setTodos((todos) => {
+      return [...todos, newTodos];
+    });
 
     // Todoを入力欄を空白に変更
     setTodoText("");
   };
 
   // 削除ボタンがおされたら
-  const onClickDelete = (id: any) => {
-    const newTodos = [...todos];
-    newTodos.splice(id, 1);
+  const onClickDelete = (id: string) => {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
     setTodos(newTodos);
   };
   return (
@@ -56,7 +64,11 @@ export const App = () => {
           placeholder="Todoを入力"
           size="md"
         />
-        <Button onClick={onClickAdd} colorScheme="blue" variant="solid">
+        <Button
+          onClick={() => onClickAdd(todo)}
+          colorScheme="blue"
+          variant="solid"
+        >
           追加
         </Button>
       </div>
@@ -71,11 +83,11 @@ export const App = () => {
       <div>
         <ul>
           {todos.map((id, text, status) => (
-            <React.Fragment key={id}>
+            <React.Fragment key={id:string}>
               <li>{text}</li>
               <Button
                 onClick={() => {
-                  onClickDelete(id);
+                  onClickDelete();
                 }}
               >
                 削除
