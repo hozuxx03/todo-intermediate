61cd5058aea242507f1c6050d5f2f4916e488573
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-20 14:43:55 +0900

    4回目

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,42 @@ import { Input } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
 
-// Todoリストの型を定義
-type TodoType = {
-  id: string;
-  text: string;
-  status: string;
-};
-
 export const App = () => {
-  // Todoを入力欄に入力された文字をstateで管理
+  // Todo入力欄のstate管理
   const [todoText, setTodoText] = useState("");
-
-  // 現在入力済みのTodoリストをstateで管理
+  // Todoリストのstate管理
   const [todos, setTodos] = useState<TodoType[]>([]);
 
-  // Todoを入力欄に何か文字が入力されたらその入力された文字に変更
-  const onChangeTodoText = (e: any) => setTodoText(e.target.value);
+  // Todoの型を定義
+  type TodoType = {
+    id: string;
+    text: string;
+    status: string;
+  };
 
-  // 追加ボタンを押した時に
-  const onClickAdd = (todo: TodoType) => {
-    // もしTodoを入力欄が空だったら処理をreturnする
+  // Todo入力欄を入力された文字に変更
+  const onChangeTodoText = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setTodoText(e.target.value);
+
+  // 追加ボタンを押した時の処理
+  const onClickAdd = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Todo入力欄が空の時の処理
     if (todoText === "") return;
-    const newTodos = {
+    // 新しいTodoを作成
+    const newTodos: TodoType = {
       id: uuidv4(),
       text: todoText,
-      status: "作業中",
+      status: "未着手",
     };
 
-    // 入力したTodoを現在入力済みのTodoリストの配列に追加
-    setTodos((todos) => {
-      return [...todos, newTodos];
-    });
+    //Todoリストの配列に追加
+    setTodos([newTodos, ...todos]);
 
     // Todoを入力欄を空白に変更
     setTodoText("");
   };
 
-  // 削除ボタンがおされたら
+  // 削除ボタンが押された時の処理
   const onClickDelete = (id: string) => {
     const newTodos = todos.filter((todo) => {
       return todo.id !== id;
@@ -55,22 +54,21 @@ export const App = () => {
       </Box>
 
       <div className="input-area">
-        <Input
-          value={todoText}
-          onChange={onChangeTodoText}
-          htmlSize={50}
-          width="90%"
-          variant="outline"
-          placeholder="Todoを入力"
-          size="md"
-        />
-        <Button
-          onClick={() => onClickAdd(todo)}
-          colorScheme="blue"
-          variant="solid"
-        >
+        <form onSubmit={(e) => onClickAdd(e)}>
+          <Input
+            type="text"
+            onChange={(e) => onChangeTodoText(e)}
+            htmlSize={50}
+            width="90%"
+            variant="outline"
+            placeholder="Todoを入力"
+            size="md"
+          />
+          <Input type="submit" value="追加" />
+        </form>
+        {/* <Button onClick={onClickAdd} colorScheme="blue" variant="solid">
           追加
-        </Button>
+        </Button> */}
       </div>
       <div className="Narrow-down-area">
         絞り込み
@@ -82,16 +80,19 @@ export const App = () => {
       </div>
       <div>
         <ul>
-          {todos.map((id, text, status) => (
-            <React.Fragment key={id:string}>
-              <li>{text}</li>
+          {todos.map((newTodos: TodoType) => (
+            <React.Fragment key={newTodos.id}>
+              <li>{newTodos.id}</li>
+              <li>{newTodos.text}</li>
+              <li>{newTodos.status}</li>
               <Button
                 onClick={() => {
-                  onClickDelete();
+                  onClickDelete(newTodos.id);
                 }}
               >
                 削除
               </Button>
+              <Button>編集</Button>
             </React.Fragment>
           ))}
         </ul>
