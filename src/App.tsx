6baa3e24384cdf51680df8cb7d49368b94cf69cb
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-20 17:53:13 +0900

    絞り込み機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -18,81 +18,99 @@ export const App = () => {
     status: string;
   };
 
-  // Todo入力欄を入力された文字に変更
   const onChangeTodoText = (e: React.ChangeEvent<HTMLInputElement>) =>
     setTodoText(e.target.value);
 
   // 追加ボタンを押した時の処理
-  const onClickAdd = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onClickAdd = (e: React.FormEvent<HTMLFormElement>) => {
+    // 追加ボタンを押した時にページがリロードしない処理
+    e.preventDefault();
     // Todo入力欄が空の時の処理
     if (todoText === "") return;
-    // 新しいTodoを作成
+    // 新しいTodoを作成の処理
     const newTodos: TodoType = {
       id: uuidv4(),
       text: todoText,
       status: "未着手",
     };
-
-    //Todoリストの配列に追加
+    //Todoリストの配列に追加の処理
     setTodos([newTodos, ...todos]);
-
-    // Todoを入力欄を空白に変更
+    // Todoを入力欄を空白に変更の処理
     setTodoText("");
   };
 
   // 削除ボタンが押された時の処理
   const onClickDelete = (id: string) => {
-    const newTodos = todos.filter((todo) => {
-      return todo.id !== id;
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  // 編集ボタンが押された時の処理
+  const onClickEdit = (id: string, text: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.text = text;
+      }
+      return todo;
     });
     setTodos(newTodos);
   };
+
   return (
     <>
-      <Box bg="blue.500" w="100%" p={15} color="white">
-        Todo List
+      <Box bg="blue.500" w="100%" p={25} color="white">
+        Todo List入力欄
       </Box>
-
       <div className="input-area">
         <form onSubmit={(e) => onClickAdd(e)}>
           <Input
             type="text"
+            value={todoText}
             onChange={(e) => onChangeTodoText(e)}
-            htmlSize={50}
-            width="90%"
+            size="md"
+            width="85%"
             variant="outline"
             placeholder="Todoを入力"
-            size="md"
           />
           <Input type="submit" value="追加" />
         </form>
-        {/* <Button onClick={onClickAdd} colorScheme="blue" variant="solid">
-          追加
-        </Button> */}
       </div>
       <div className="Narrow-down-area">
         絞り込み
-        <Select placeholder="全て" width="95%">
+        <Select placeholder="全て" width="15%">
           <option value="未着手">未着手</option>
           <option value="着手">着手</option>
           <option value="完了">完了</option>
         </Select>
       </div>
+      <Box bg="blue.500" w="100%" p={15} color="white">
+        TodoList一覧
+      </Box>
       <div>
         <ul>
-          {todos.map((newTodos: TodoType) => (
-            <React.Fragment key={newTodos.id}>
-              <li>{newTodos.id}</li>
-              <li>{newTodos.text}</li>
-              <li>{newTodos.status}</li>
+          {todos.map((todo) => (
+            <React.Fragment>
+              <li key={todo.id}>
+                <Input
+                  value={todo.text}
+                  type="text"
+                  onChange={(e) => onClickEdit(todo.id, e.target.value)}
+                  size="md"
+                  width="85%"
+                  variant="outline"
+                />
+                <Select placeholder="未着手" width="15%">
+                  <option value="着手">着手</option>
+                  <option value="完了">完了</option>
+                </Select>
+              </li>
               <Button
                 onClick={() => {
-                  onClickDelete(newTodos.id);
+                  onClickDelete(todo.id);
                 }}
               >
                 削除
               </Button>
-              <Button>編集</Button>
             </React.Fragment>
           ))}
         </ul>
