6f83e6fec5748c93153cbf91ab04cd68ecff1444
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-11-13 16:19:21 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,20 @@ import { Input } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
 
-export const App = () => {
-  // Todoの型を定義
-  type TodoType = {
-    id: string;
-    text: string;
-    status: string;
-  };
+// Todoの型を定義
+type TodoType = {
+  id: string;
+  text: string;
+  status: string;
+};
 
+export const App = () => {
   // Todo入力欄のstate管理
   const [todoText, setTodoText] = useState("");
   // Todoリストのstate管理
   const [todos, setTodos] = useState<TodoType[]>([]);
   // フィルターのstate管理
-  const [filter, setFilter] = useState("未着手");
+  const [filter, setFilter] = useState("全て");
   // 絞り込まれたTodoリストのstate管理
   const [filteredTodos, setFilteredTodos] = useState<TodoType[]>([]);
 
@@ -70,7 +70,7 @@ export const App = () => {
   }, [filter, todos]);
 
   // 編集ボタンが押された時の処理
-  const onClickEdit = (id: string, text: string) => {
+  const onChangeEdit = (id: string, text: string) => {
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.text = text;
@@ -81,17 +81,14 @@ export const App = () => {
   };
   // Todoリストのステータスが変更された時の処理
   const handleStatusChange = (
-    targetTodo: { id: string; text: string; status: string },
+    targetTodo: TodoType,
     e: React.ChangeEvent<HTMLSelectElement>
   ) => {
-    console.log(targetTodo, e);
-
     // Todoリストを更新
     const newArray = todos.map((todo) =>
       todo.id === targetTodo.id ? { ...todo, status: e.target.value } : todo
     );
     setTodos(newArray);
-    console.log(newArray);
   };
 
   return (
@@ -118,7 +115,6 @@ export const App = () => {
         <Select
           value={filter}
           onChange={(e) => setFilter(e.target.value)}
-          defaultValue="全て"
           width="15%"
         >
           <option value="全て">全て</option>
@@ -133,27 +129,24 @@ export const App = () => {
       <div>
         <ul>
           {filteredTodos.map((todo) => (
-            <React.Fragment>
-              <li key={todo.id}>
-                <Input
-                  value={todo.text}
-                  type="text"
-                  onChange={(e) => onClickEdit(todo.id, e.target.value)}
-                  size="md"
-                  width="85%"
-                  variant="outline"
-                />
-                <Select
-                  value={todo.status}
-                  // placeholder="未着手"
-                  width="15%"
-                  onChange={(e) => handleStatusChange(todo, e)}
-                >
-                  <option value="未着手">未着手</option>
-                  <option value="着手">着手</option>
-                  <option value="完了">完了</option>
-                </Select>
-              </li>
+            <React.Fragment key={todo.id}>
+              <Input
+                value={todo.text}
+                type="text"
+                onChange={(e) => onChangeEdit(todo.id, e.target.value)}
+                size="md"
+                width="85%"
+                variant="outline"
+              />
+              <Select
+                value={todo.status}
+                width="15%"
+                onChange={(e) => handleStatusChange(todo, e)}
+              >
+                <option value="未着手">未着手</option>
+                <option value="着手">着手</option>
+                <option value="完了">完了</option>
+              </Select>
               <Button
                 onClick={() => {
                   onClickDelete(todo.id);
